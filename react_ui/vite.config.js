f0238a7202afc1c6791ac0fbda5ff2dff5d47b30
ferdeveloper05 <fer.developer05@gmail.com>
2026-06-17 23:24:53 -0400

    Probando la integracion de la API con la interfaz de React

--- react_ui/vite.config.js
+++ react_ui/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8100',
+        target: 'http://localhost:8000', /*Cambiar al puerto 8100*/
         changeOrigin: true,
       }
     }
